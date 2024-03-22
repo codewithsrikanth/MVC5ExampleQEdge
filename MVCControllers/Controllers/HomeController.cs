e2b7dca2b2ec84f8acfa7998dde1589bd4390a99
Srikanth Rallabandi <codewithsrikanth@gmail.com>
2024-03-22 07:57:06 +0530

    Add project files.

---
+++ MVCControllers/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MVCControllers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Aboutus()
+        {
+            return View();
+        }
+        public ActionResult Contactus()
+        {
+            return View();
+        }
+        public ActionResult Blogs()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCControllers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCControllers.MvcApplication" Language="C#" %>
