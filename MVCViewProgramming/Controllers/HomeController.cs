e932e2c2aef62a8fd9a145c26696fe047c0a76fe
Srikanth Rallabandi <codewithsrikanth@gmail.com>
2024-03-25 09:44:22 +0530

    Views in ASP.Net MVC5

---
+++ MVCViewProgramming/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace MVCViewProgramming.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+        public ActionResult Index3()
+        {
+            return View();
+        }
+        public ActionResult Index4()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCViewProgramming/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCViewProgramming.MvcApplication" Language="C#" %>
