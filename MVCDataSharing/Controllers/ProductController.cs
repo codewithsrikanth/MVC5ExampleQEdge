9ad9b1ab0a7bc0b4ce026cbbea82b12bbb9d1338
Srikanth Rallabandi <codewithsrikanth@gmail.com>
2024-03-27 09:27:40 +0530

    PostAction Method Parameters and Bootstrap Integration

---
+++ MVCDataSharing/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using MVCDataSharing.Models;
+using System;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace MVCDataSharing.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult GetProductInfo()
+        {
+            return View();
+        }
+
+        //Form Collection
+        //[HttpPost]
+        //public ActionResult GetProductInfo(FormCollection frmObj)
+        //{
+        //    int id = Convert.ToInt16(frmObj["productId"]);
+        //    string name = frmObj["productName"];
+        //    double price =Convert.ToDouble(frmObj["productCost"]);
+        //    Response.Write($"{id}  -  {name}  -  {price}");            
+        //    return View();
+        //}
+
+
+
+        //Formal Parameter
+        //[HttpPost]
+        //public ActionResult GetProductInfo(int productId, string ProductName, double productCost)
+        //{
+        //    Response.Write($"{productId}    -   {ProductName}   -   {productCost}");
+        //    return View();
+        //}
+
+        //Model Class Object
+        [HttpPost]
+        public ActionResult GetProductInfo(Product obj)
+        {
+            Response.Write($"{obj.ProductId}    -   {obj.ProductName}   -   {obj.ProductCost}");
+            return View();
+        }
+
+        public ActionResult GetProducts()
+        {
+            return View();
+        }
+        public ContentResult GetProductName(string id)
+        {
+            return Content("Product Name is: " + id);
+        }
+        public ContentResult GetNameAndPrice(string name, double price)
+        {
+            string info = $"ProductName is: {name} and Price is: {price}";
+            return Content(info);
+        }
+    }
+}
---
+++ MVCDataSharing/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCDataSharing.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double ProductCost { get; set; }
+    }
+}
