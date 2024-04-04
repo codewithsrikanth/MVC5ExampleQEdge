f1d0df41ef80d4af21c9a322585c05e33c88f124
Srikanth Rallabandi <codewithsrikanth@gmail.com>
2024-04-04 09:22:17 +0530

    Partial Views and Layout Views

---
+++ PartialAndLayoutViews/Controllers/DeptController.cs
@@ -0,0 +1,123 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using PartialAndLayoutViews.Models;
+
+namespace PartialAndLayoutViews.Controllers
+{
+    public class DeptController : Controller
+    {
+        private CompanyDBEntities db = new CompanyDBEntities();
+
+        // GET: Dept
+        public ActionResult Index()
+        {
+            return View(db.Depts.ToList());
+        }
+
+        // GET: Dept/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dept dept = db.Depts.Find(id);
+            if (dept == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dept);
+        }
+
+        // GET: Dept/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dept/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DeptID,DeptName")] Dept dept)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Depts.Add(dept);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(dept);
+        }
+
+        // GET: Dept/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dept dept = db.Depts.Find(id);
+            if (dept == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dept);
+        }
+
+        // POST: Dept/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DeptID,DeptName")] Dept dept)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dept).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(dept);
+        }
+
+        // GET: Dept/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dept dept = db.Depts.Find(id);
+            if (dept == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dept);
+        }
+
+        // POST: Dept/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Dept dept = db.Depts.Find(id);
+            db.Depts.Remove(dept);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PartialAndLayoutViews/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using PartialAndLayoutViews.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace PartialAndLayoutViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CompanyDBEntities dbContext = new CompanyDBEntities();
+        public ActionResult Index()
+        {
+            List<Employee> emps =  dbContext.Employees.ToList();
+            return View(emps);
+        }
+        public ActionResult EmpData()
+        {
+            List<Employee> emps = dbContext.Employees.ToList();
+            return View(emps);
+        }
+        public ActionResult GetSingleEmp()
+        {
+            Employee emp = dbContext.Employees.Find(101);
+            return View(emp);
+        }
+    }
+}
