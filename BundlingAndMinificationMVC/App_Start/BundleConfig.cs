df8f193bb1dd89639aa8ccc84bf3be7ab518edbf
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-04-17 08:59:24 +0530

    Bundling and Minification

---
+++ BundlingAndMinificationMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Optimization;
+
+namespace BundlingAndMinificationMVC.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundle(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Sample/javascript")
+                .Include("~/Scripts/bootstrap.bundle.js",
+                "~/Scripts/bootstrap.bundle.js.map",
+                "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Sample/css")
+                    .Include("~/Content/bootstrap-grid.css")
+                    .Include("~/Content/bootstrap.css")
+                    .Include("~/Content/bootstrap.rtl.css")
+                );
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
